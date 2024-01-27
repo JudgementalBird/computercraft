c39363438385cd478c3e1b046c75748dd2b7b358
JudgementalBird <judgementalbird@gmail.com>
2024-01-27 17:05:52 +0100

    Update pull.lua

--- pull.lua
+++ pull.lua
@@ -1,8 +1,7 @@
 --[[
 todo
-implement cut feature
 
-make it so if you try to add a URL thats already in the csv then it wont let you
+implement cut feature
 
 make it spam you less with garbage
 make it use colored text
@@ -22,19 +21,42 @@ function handleurl(theurl)
                   
                   print("http success, file downloaded")
                   responsestring = response.readAll()
+                  
                   if usedurl:find("%.lua") then--escape the . with %, effectively "if .lua is in the string then:"
                         filename = usedurl:match("([^/]-)$")
                   else
                         filename = "unknown_download"
                   end
+                  
+                  local allfiles = fs.list("")
+                  local foundexisting = {}
+                  for k,v in pairs(allfiles) do--for every path in the top directory:
+                        if v:find(filename) then--if chosen filename is found in this path
+                              foundexisting[#foundexisting+1] = v--add path to foundexisting table
+                        end
+                  end
+                  if #foundexisting == 1 then
+                        print("found file by same name, deleting")
+                        result = fs.delete(foundexisting[1])
+                        print(tostring(result))
+                  elseif #foundexisting > 1 then--more than 1 existing files with that name, stop and let the user figure it out
+                        print("error: found multiple files by same name:")
+                        for k,v in ipairs(foundexisting) do
+                              print(v)
+                        end
+                        error("stopping")
+                  end
+                        
                   print("installing file with filename: "..filename)
                   tempwriter = io.open(filename,"w")
                   tempwriter:write(responsestring)
                   requested = false
+
             elseif event == "http_failure" then
 
                   print("http failure")
                   requested = false 
+
             end
       end
 end
@@ -79,12 +101,21 @@ elseif args[1] == "all" then --user wants to pull from every url in pull csv
       end
 
 elseif args[1] == "add" then --user is just adding to the pull csv
+      --read the csv to a string to compare input
+      local tempread = fs.open("pulldir/pullcsv","r")
+      local csvstring = tempread.readAll()
+      tempread:close()
+
       if type(args[2]) == "nil" then
             print("missing second arg after 'add'")
       else
-            print("adding the following to pullcsv: "..args[2]..",")    
-            pullcsv = io.open("pulldir/pullcsv","a")
-            pullcsv:write((args[2]..","))
+            if not (csvstring:find(args[2])) then
+                  print("adding the following to pullcsv: "..args[2]..",")    
+                  pullcsv = io.open("pulldir/pullcsv","a")
+                  pullcsv:write((args[2]..","))
+            else
+                  print("supplied url was found in csv, not adding")
+            end
       end
 
 elseif args[1] == "cut" then --user wants to remove one url from pull csv
