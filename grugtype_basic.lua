96769527866dbbe95ee190cad90e2556a96508d2
JudgementalBird <judgementalbird@gmail.com>
2024-01-28 03:39:17 +0100

    Update grugtype_basic.lua

--- grugtype_basic.lua
+++ grugtype_basic.lua
@@ -7,6 +7,7 @@ color = {
       white = 0x1,
       black = 0x8000,
       lgray = 0x100,
+      dgray = 0x80,
       background = term.getBackgroundColor(),
       path = 0x80
 }
@@ -18,4 +19,6 @@ term.setTextColor(color.black)
 term.setCursorPos(1,1)
 version = 1
 term.setTextColor(color.lgray)
-term.write("v "..version)
+term.write("v "..version.."   ")
+term.setTextColor(color.dgray)
+term.write("Grugtype Basic")
